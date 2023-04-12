85031c38fcbcf07b1cf71a29799d4218c40ada6b
muhammadvadud <gmuhammadvadud@gmail.com>
2023-04-12 14:57:22 +0500

    first commit

---
+++ Blog2/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.views.static import serve
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include("blogapp.urls")),
+    re_path(r"media/(.*)",serve,{"document_root":settings.MEDIA_ROOT}),
+    re_path(r"static/(.*)", serve, {"document_root": settings.STATIC_ROOT})
+
+]
